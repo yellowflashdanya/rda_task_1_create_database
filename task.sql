bb03c2dbeeb12931cba242e12f2d3d1d25648616
Danylo Melnychuk <danyakid2005@gmail.com>
2025-04-25 01:10:30 +0300

    modified task.sql with creating database and tables

--- task.sql
+++ task.sql
@@ -1,1 +1,32 @@
-# Write your SQL code for the database creation here. Good luck! 
+CREATE DATABASE IF NOT EXISTS ShopDB;
+USE ShopDB;
+
+CREATE TABLE Products (
+	ID INT AUTO_INCREMENT PRIMARY KEY,
+    NAME VARCHAR(100) NOT NULL,
+    Description VARCHAR(100),
+    Price INT NOT NULL,
+    WarehouseAmount INT NOT NULL
+);
+
+CREATE TABLE Customers (
+	ID INT AUTO_INCREMENT PRIMARY KEY,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Email VARCHAR(100) NOT NULL,
+    Address VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Orders (
+	ID INT AUTO_INCREMENT PRIMARY KEY,
+    CustomerID INT,
+    Date DATE NOT NULL
+);
+
+CREATE TABLE OrderItems (
+	ID INT AUTO_INCREMENT PRIMARY KEY,
+    OrderID INT,
+    ProductID INT,
+    FOREIGN KEY (OrderID) REFERENCES Orders(ID) ON DELETE SET NULL,
+    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE SET NULL
+);
